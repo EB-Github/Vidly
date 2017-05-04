3f5de598691c806152a6d850ff7d343ae1cac52c
ebrown6 <ebrown6@uab.edu>
2017-05-03 23:10:52 -0500

    Added Comments

--- Vidly/Controllers/CustomersController.cs
+++ Vidly/Controllers/CustomersController.cs
@@ -10,22 +10,23 @@ namespace Vidly.Controllers
 {
     public class CustomersController : Controller
     {
+        //Temporary- hardcoded customers for testing purposes
         List<Customer>customers = new List<Customer>()
-            {
-                new Customer {Name = "Freddy Joe", Id=1 },
-                new Customer {Name = "Billie Jean", Id=2 }
-            };
-        // GET: Customers
+        {
+            new Customer {Name = "Freddy Joe", Id=1},
+            new Customer {Name = "Billie Jean", Id=2}
+        };
+        // GET: Customers/Index
         public ActionResult Index()
         {
-            
-
             var viewModel = new ViewModelCustomers()
             {
                 Customers = customers
             };
             return View(viewModel);
         }
+
+        //GET Customers/Details/{ID}
         public ActionResult Details(int id)
         {
             var customer = customers.Single(Customer => Customer.Id == id);
--- Vidly/Controllers/MoviesController.cs
+++ Vidly/Controllers/MoviesController.cs
@@ -9,18 +9,19 @@ namespace Vidly.Controllers
 {
     public class MoviesController : Controller
     {
+        //Temporary- Hardcoded Movies for testing
         List<Movie> movies = new List<Movie>
        {
            new Movie {Id=1,Name="Shrek!"},
            new Movie {Id=2,Name="Wall-e" }
        };
-
+        //GET Movies/Details/{ID}
         public ActionResult Details(int id)
         {
             var movie = movies.Single(Movie => Movie.Id == id);
             return View(movie);
         }
-
+        //GET Movies/Index
         public ActionResult Index()
         {
             return View()
