9626f3736ea55221da13ce9483e6f37d37e842a7
ebrown6 <ebrown6@uab.edu>
2017-05-03 23:05:35 -0500

    First Commit- Day 1

---
+++ Vidly/Controllers/CustomersController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Vidly.Models;
+using Vidly.ViewModels;
+using System.Web.Mvc;
+
+namespace Vidly.Controllers
+{
+    public class CustomersController : Controller
+    {
+        List<Customer>customers = new List<Customer>()
+            {
+                new Customer {Name = "Freddy Joe", Id=1 },
+                new Customer {Name = "Billie Jean", Id=2 }
+            };
+        // GET: Customers
+        public ActionResult Index()
+        {
+            
+
+            var viewModel = new ViewModelCustomers()
+            {
+                Customers = customers
+            };
+            return View(viewModel);
+        }
+        public ActionResult Details(int id)
+        {
+            var customer = customers.Single(Customer => Customer.Id == id);
+            return View(customer);
+        }
+    }
+}
---
+++ Vidly/Controllers/MoviesController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Vidly.Models;
+using Vidly.ViewModels;
+namespace Vidly.Controllers
+{
+    public class MoviesController : Controller
+    {
+        List<Movie> movies = new List<Movie>
+       {
+           new Movie {Id=1,Name="Shrek!"},
+           new Movie {Id=2,Name="Wall-e" }
+       };
+
+        public ActionResult Details(int id)
+        {
+            var movie = movies.Single(Movie => Movie.Id == id);
+            return View(movie);
+        }
+
+        public ActionResult Index()
+        {
+            return View()
+        }
+    }
+}
